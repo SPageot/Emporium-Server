8f7ce22f9699036fa543449cd311be7178eec91d
Stanley Pageot <stanley@rxdu.io>
2022-08-02 16:17:21 -0700

    update backend

--- app.js
+++ app.js
@@ -2,15 +2,26 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const PORT = process.env.PORT || 3010;
 
 app.use(cors());
+app.use(express.json());
 
+const findMovieTrailer = async (videoName) => {
+  const yts = require("yt-search");
+  const r = await yts(`${videoName} trailer`);
 
+  const videos = r.videos.slice(0, 1);
 
-app.post('/movies', (res, req) => {
-    console.log(req.body)
-})
+  return videos[0].url;
+};
 
-app.listen(process.env.PORT, () => {
-    console.log(`server started on PORT: ${process.env.PORT}`)
+app.post("/movies", async (req, res) => {
+  res.json({
+    urlLink: await findMovieTrailer(req.body.movie),
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`server started on PORT: ${process.env.PORT}`);
 });
