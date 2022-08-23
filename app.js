200a3ed500c57e362b82776777abbb0d340f631f
Stanley Pageot <stanley@rxdu.io>
2022-08-22 20:01:36 -0700

    move api key

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
+const axios = require("axios");
 const express = require("express");
 const cors = require("cors");
+const { response } = require("express");
 const app = express();
 const PORT = process.env.PORT || 3010;
 
@@ -16,9 +18,17 @@ const findMovieTrailer = async (videoName) => {
   return videos[0].url;
 };
 
-app.post("/movies", async (req, res) => {
+app.get("/movies", async (req, res) => {
+  await axios
+    .post(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.API_KEY}&language=en-US&page=1`
+    )
+    .then((response) => res.send(response?.data));
+});
+
+app.post("/movies/movie", async (req, res) => {
   res.json({
-    urlLink: await findMovieTrailer(req.body.movie),
+    urlLink: await findMovieTrailer(req?.body?.movie),
   });
 });
 
